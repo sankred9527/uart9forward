972be98da77091af4dc472310ba0f9e715aa451e
fallshuang <falls.huang@gmail.com>
2024-02-09 21:50:02 +0800

    import code

---
+++ Inc/main.h
@@ -0,0 +1,155 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RTS2_0_Pin GPIO_PIN_1
+#define RTS2_0_GPIO_Port GPIOA
+#define TXD2_0_Pin GPIO_PIN_2
+#define TXD2_0_GPIO_Port GPIOA
+#define RXD2_0_Pin GPIO_PIN_3
+#define RXD2_0_GPIO_Port GPIOA
+#define LG1_Pin GPIO_PIN_4
+#define LG1_GPIO_Port GPIOA
+#define LG2_Pin GPIO_PIN_5
+#define LG2_GPIO_Port GPIOA
+#define LG3_Pin GPIO_PIN_6
+#define LG3_GPIO_Port GPIOA
+#define LG4_Pin GPIO_PIN_7
+#define LG4_GPIO_Port GPIOA
+#define LG5_Pin GPIO_PIN_0
+#define LG5_GPIO_Port GPIOB
+#define LG6_Pin GPIO_PIN_1
+#define LG6_GPIO_Port GPIOB
+#define BR0_Pin GPIO_PIN_12
+#define BR0_GPIO_Port GPIOE
+#define BR1_Pin GPIO_PIN_13
+#define BR1_GPIO_Port GPIOE
+#define RSV0_Pin GPIO_PIN_14
+#define RSV0_GPIO_Port GPIOE
+#define RSV1_Pin GPIO_PIN_15
+#define RSV1_GPIO_Port GPIOE
+#define TXD3_0_Pin GPIO_PIN_10
+#define TXD3_0_GPIO_Port GPIOB
+#define RXD3_0_Pin GPIO_PIN_11
+#define RXD3_0_GPIO_Port GPIOB
+#define LR1_Pin GPIO_PIN_12
+#define LR1_GPIO_Port GPIOB
+#define LR2_Pin GPIO_PIN_13
+#define LR2_GPIO_Port GPIOB
+#define RTS3_0_Pin GPIO_PIN_14
+#define RTS3_0_GPIO_Port GPIOB
+#define LR3_Pin GPIO_PIN_15
+#define LR3_GPIO_Port GPIOB
+#define LR4_Pin GPIO_PIN_10
+#define LR4_GPIO_Port GPIOD
+#define LR5_Pin GPIO_PIN_11
+#define LR5_GPIO_Port GPIOD
+#define LR6_Pin GPIO_PIN_13
+#define LR6_GPIO_Port GPIOD
+#define LR7_Pin GPIO_PIN_14
+#define LR7_GPIO_Port GPIOD
+#define LR8_Pin GPIO_PIN_15
+#define LR8_GPIO_Port GPIOD
+#define RTS4_0S_Pin GPIO_PIN_8
+#define RTS4_0S_GPIO_Port GPIOC
+#define RTS4_1S_Pin GPIO_PIN_9
+#define RTS4_1S_GPIO_Port GPIOC
+#define BEEP_Pin GPIO_PIN_8
+#define BEEP_GPIO_Port GPIOA
+#define RTS1_1S_Pin GPIO_PIN_11
+#define RTS1_1S_GPIO_Port GPIOA
+#define RTS1_0_Pin GPIO_PIN_12
+#define RTS1_0_GPIO_Port GPIOA
+#define TXD4_Pin GPIO_PIN_10
+#define TXD4_GPIO_Port GPIOC
+#define RXD4_Pin GPIO_PIN_11
+#define RXD4_GPIO_Port GPIOC
+#define TXD5_Pin GPIO_PIN_12
+#define TXD5_GPIO_Port GPIOC
+#define LR9_Pin GPIO_PIN_0
+#define LR9_GPIO_Port GPIOD
+#define PWR1_Pin GPIO_PIN_1
+#define PWR1_GPIO_Port GPIOD
+#define RXD5_Pin GPIO_PIN_2
+#define RXD5_GPIO_Port GPIOD
+#define RTS5S_Pin GPIO_PIN_3
+#define RTS5S_GPIO_Port GPIOD
+#define LG7_Pin GPIO_PIN_3
+#define LG7_GPIO_Port GPIOB
+#define LG8_Pin GPIO_PIN_4
+#define LG8_GPIO_Port GPIOB
+#define LG9_Pin GPIO_PIN_5
+#define LG9_GPIO_Port GPIOB
+#define TXD1_0_Pin GPIO_PIN_6
+#define TXD1_0_GPIO_Port GPIOB
+#define RXD1_0_Pin GPIO_PIN_7
+#define RXD1_0_GPIO_Port GPIOB
+#define PWR2_Pin GPIO_PIN_8
+#define PWR2_GPIO_Port GPIOB
+#define PWR3_Pin GPIO_PIN_9
+#define PWR3_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Src/main.c
@@ -0,0 +1,510 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "delay.h"
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+GPIO_TypeDef* GPIO_TypeList[] = {GPIOA,GPIOB,GPIOC,GPIOD,GPIOE};
+
+const uint16_t PLeds[3][2] = {{3,PWR1_Pin},{1,PWR2_Pin},{1,PWR3_Pin}};
+const uint16_t RLeds[9][2] = {{1,LR1_Pin},{1,LR2_Pin},{1,LR3_Pin},{3,LR4_Pin},{3,LR5_Pin},{3,LR6_Pin},{3,LR7_Pin},{3,LR8_Pin},{3,LR9_Pin}};
+const uint16_t GLeds[9][2] = {{0,LG1_Pin},{0,LG2_Pin},{0,LG3_Pin},{0,LG4_Pin},{1,LG5_Pin},{1,LG6_Pin},{1,LG7_Pin},{1,LG8_Pin},{1,LG9_Pin}};
+
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart4;
+UART_HandleTypeDef huart5;
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart3;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_UART4_Init(void);
+static void MX_UART5_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART3_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+
+//  ledtype:    0:PLeds    1:RLeds    2:GLeds
+void SetLed(uint8_t ledtype, uint8_t ledidx, uint8_t status)
+{
+	switch (ledtype)
+	{
+		case 0:
+		{
+			if(ledidx >= 3) return;
+			HAL_GPIO_WritePin(GPIO_TypeList[PLeds[ledidx][0]], PLeds[ledidx][1], status > 0 ? GPIO_PIN_RESET : GPIO_PIN_SET);
+		}break;
+		case 1:
+		{
+			if(ledidx >= 9) return;
+			HAL_GPIO_WritePin(GPIO_TypeList[RLeds[ledidx][0]], RLeds[ledidx][1], status > 0 ? GPIO_PIN_RESET : GPIO_PIN_SET);
+		}break;
+		case 2:
+		{
+			if(ledidx >= 9) return;
+			HAL_GPIO_WritePin(GPIO_TypeList[GLeds[ledidx][0]], GLeds[ledidx][1], status > 0 ? GPIO_PIN_RESET : GPIO_PIN_SET);
+		}break;
+	}
+}
+
+void  Uart5SendString(char* str)
+{
+  HAL_GPIO_WritePin(RTS5S_GPIO_Port, RTS5S_Pin, GPIO_PIN_SET);
+	HAL_UART_Transmit(&huart5,(uint8_t*)str,strlen(str),50);
+  HAL_GPIO_WritePin(RTS5S_GPIO_Port, RTS5S_Pin, GPIO_PIN_RESET);
+}
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  delay_init(32);
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_UART4_Init();
+  MX_UART5_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+  uint8_t ct0 = 0,ct1 = 0;
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	SetLed(0,ct0,0);
+	ct0++;
+	if(ct0 >= 3) ct0 = 0;
+	SetLed(0,ct0,1);
+
+
+	//	SetLed(1,ct1,1);
+	//	SetLed(1,ct1,1);
+	SetLed(1,ct1,0);
+	SetLed(2,8-ct1,0);
+	ct1++;
+	if(ct1 >= 9) ct1 = 0;
+	SetLed(1,ct1,1);
+	SetLed(2,8-ct1,1);
+
+	delay_ms(100);
+	Uart5SendString("Hello World\r\n");
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief UART4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_UART4_Init(void)
+{
+
+  /* USER CODE BEGIN UART4_Init 0 */
+
+  /* USER CODE END UART4_Init 0 */
+
+  /* USER CODE BEGIN UART4_Init 1 */
+
+  /* USER CODE END UART4_Init 1 */
+  huart4.Instance = UART4;
+  huart4.Init.BaudRate = 115200;
+  huart4.Init.WordLength = UART_WORDLENGTH_8B;
+  huart4.Init.StopBits = UART_STOPBITS_1;
+  huart4.Init.Parity = UART_PARITY_NONE;
+  huart4.Init.Mode = UART_MODE_TX_RX;
+  huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart4.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART4_Init 2 */
+
+  /* USER CODE END UART4_Init 2 */
+
+}
+
+/**
+  * @brief UART5 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_UART5_Init(void)
+{
+
+  /* USER CODE BEGIN UART5_Init 0 */
+
+  /* USER CODE END UART5_Init 0 */
+
+  /* USER CODE BEGIN UART5_Init 1 */
+
+  /* USER CODE END UART5_Init 1 */
+  huart5.Instance = UART5;
+  huart5.Init.BaudRate = 115200;
+  huart5.Init.WordLength = UART_WORDLENGTH_8B;
+  huart5.Init.StopBits = UART_STOPBITS_1;
+  huart5.Init.Parity = UART_PARITY_NONE;
+  huart5.Init.Mode = UART_MODE_TX_RX;
+  huart5.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart5.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART5_Init 2 */
+
+  /* USER CODE END UART5_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_RTS;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_RTS;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_RTS;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LG1_Pin|LG2_Pin|LG3_Pin|LG4_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LG5_Pin|LG6_Pin|LR1_Pin|LR2_Pin
+                          |LR3_Pin|LG7_Pin|LG8_Pin|LG9_Pin
+                          |PWR2_Pin|PWR3_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, LR4_Pin|LR5_Pin|LR6_Pin|LR7_Pin
+                          |LR8_Pin|LR9_Pin|PWR1_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, RTS4_0S_Pin|RTS4_1S_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, BEEP_Pin|RTS1_1S_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(RTS5S_GPIO_Port, RTS5S_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LG1_Pin LG2_Pin LG3_Pin LG4_Pin */
+  GPIO_InitStruct.Pin = LG1_Pin|LG2_Pin|LG3_Pin|LG4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LG5_Pin LG6_Pin LR1_Pin LR2_Pin
+                           LR3_Pin LG7_Pin LG8_Pin LG9_Pin
+                           PWR2_Pin PWR3_Pin */
+  GPIO_InitStruct.Pin = LG5_Pin|LG6_Pin|LR1_Pin|LR2_Pin
+                          |LR3_Pin|LG7_Pin|LG8_Pin|LG9_Pin
+                          |PWR2_Pin|PWR3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BR0_Pin BR1_Pin RSV0_Pin RSV1_Pin */
+  GPIO_InitStruct.Pin = BR0_Pin|BR1_Pin|RSV0_Pin|RSV1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LR4_Pin LR5_Pin LR6_Pin LR7_Pin
+                           LR8_Pin LR9_Pin PWR1_Pin */
+  GPIO_InitStruct.Pin = LR4_Pin|LR5_Pin|LR6_Pin|LR7_Pin
+                          |LR8_Pin|LR9_Pin|PWR1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RTS4_0S_Pin RTS4_1S_Pin */
+  GPIO_InitStruct.Pin = RTS4_0S_Pin|RTS4_1S_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BEEP_Pin RTS1_1S_Pin */
+  GPIO_InitStruct.Pin = BEEP_Pin|RTS1_1S_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : RTS5S_Pin */
+  GPIO_InitStruct.Pin = RTS5S_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(RTS5S_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
